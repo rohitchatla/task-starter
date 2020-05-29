691f14eef722538cf3ce42c237d7d2a2dd4adbb1
CV Rohit <51963891+rohitchatla@users.noreply.github.com>
2020-05-29 13:54:07 +0530

    Add files via upload

---
+++ routes/lane.js
@@ -0,0 +1,53 @@
+
+//lib/packages imports
+const express=require('express');
+const router=express.Router();
+const Post=require('../models/Post');
+const User=require('../models/User');
+const Lane=require('../models/lane');
+
+//get specific person details
+router.get('/:id',async(req,res)=>{
+	try{
+		const post=await Lane.findById(req.params.id);
+		res.json(post);
+	}catch(err){
+
+		res.json({message:err});
+	}
+});
+
+
+//add lane with the uid
+router.post('/add', async (req,res)=>{//add the lane to the current user's(uid) project(pid) 
+	const post = new Lane({
+		'contributors.id' : req.body.uid,
+		title : req.body.title,
+		description : req.body.description,
+		'pid.id' : req.body.pid
+	});
+
+	try{
+	const savedPost=await post.save();
+	res.json({message:'Lane added'});
+	
+	}catch(err) {
+		res.json({message: err});
+	}
+
+});
+
+
+//add a new phase in lane(lid)
+router.post('/node/add', async (req,res)=>{
+	try{
+	const updatedPost=await Lane.update({_id:req.body.lid},
+		{$push: {'node.msg':req.body.title}});
+	res.json({message:'lane details updated'});
+	}catch(err){
+		res.json({message:err});
+	}
+
+});
+
+module.exports=router;
